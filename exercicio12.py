46660fb832689a7452600a0d34942f4478a74385
AndrePX789 <andrepx_oliveira@hotmail.com>
2020-11-20 12:23:37 -0300

    novo exercicio

--- exercicio12.py
+++ exercicio12.py
@@ -1,4 +1,12 @@
-int1 = int(input("Digite um Número Inteiro"))
-int2 = int(input("Digite outro número Inteiro"))
+int1 = int(input("Digite um Número Inteiro "))
+int2 = int(input("Digite outro número Inteiro "))
 
 lista = []
+def intervalo (num1, num2):
+    lista = []
+    for numero in range(num1, num2 + 1):
+        lista.append(numero)
+    return lista
+
+lista = intervalo(int1, int2)
+print (lista)
